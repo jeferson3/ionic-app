30421486fd8b6a42789bc900a4b334203775475c
jeferson <jefersongomes333@hotmail.com>
2020-08-15 17:53:01 -0300

    update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
   items = [
-    { id:1, name: 'teste', value: 123 },
-    { id:2, name: 'teste2', value: 123 },
-    { id:3, name: 'teste3', value: 123 },
+    { id: 1, name: 'teste', value: 123 },
+    { id: 2, name: 'teste2', value: 123 },
+    { id: 3, name: 'teste3', value: 123 },
   ]
   constructor() { }
+  doRefresh(event) {
+    setTimeout(() => {
+      event.target.complete();
+    }, 2000);
+  }
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
 
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.items.length == 1000) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  }
 }
